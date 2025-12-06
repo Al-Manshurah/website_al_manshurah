4ee8eadf9ffe7869295a74be08189014a76cd838
Zen Zalepik <51511976+zenzalepik@users.noreply.github.com>
2025-12-06 18:09:29 +0700

    fix: integrate font in production mode

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,25 @@
 // ⛔️ JANGAN import komponen client di sini
+import { Inter, Kufam, Lora } from "next/font/google";
 import "./styles/globals.css";
 import Footer from "@/components/Footer";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+  display: "swap",
+});
+
+const kufam = Kufam({
+  subsets: ["latin"],
+  variable: "--font-kufam",
+  display: "swap",
+});
+
+const lora = Lora({
+  subsets: ["latin"],
+  variable: "--font-lora",
+  display: "swap",
+});
 
 export const metadata = {
   title: "Yayasan Al Manshurah",
@@ -10,7 +29,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="id">
-      <body className="font-sans bg-white text-gray-800">
+      <body
+        className={`${inter.variable} ${kufam.variable} ${lora.variable} font-sans bg-white text-gray-800`}
+      >
         {children}
         <Footer />
       </body>
